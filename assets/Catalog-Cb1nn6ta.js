e3281168f61248c61234ab0ffae42c2508c55d5c
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2024-04-29 09:40:49 +0000

    Deploying to gh-pages from @ batalova-kira/product-cards@6406781e69f23d602e151b9970314a84a9621d8d 🚀

---
+++ assets/Catalog-Cb1nn6ta.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CL5a3flR.js";const o=()=>t.jsx("div",{children:"Catalog"});export{o as default};
---
+++ assets/Favorites-CUqSTkO_.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CL5a3flR.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
